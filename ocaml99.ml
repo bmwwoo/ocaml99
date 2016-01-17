11e120529620a80ba282f1cfeaa06b222f89273a
Brandon Woo <bmw.woo@gmail.com>
2016-01-16 20:22:47 -0800

    Finish last two

--- ocaml99.ml
+++ ocaml99.ml
@@ -10,3 +10,12 @@ let rec last x = match x with
               else last b
 ;;
 
+(* Find the last but one (last and penultimate) elements of a list. *)
+
+let rec last_two = function
+  | [] -> None
+  | [_] -> None
+  | [a;b] -> Some (a, b)
+  | _ :: b -> last_two b
+;;
+
